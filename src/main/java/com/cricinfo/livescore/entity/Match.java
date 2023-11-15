fb2f37205459fb0cbca848619336968195b59ba5
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-15 18:48:43 +0530

    Update Match.java

--- src/main/java/com/cricinfo/livescore/entity/Match.java
+++ src/main/java/com/cricinfo/livescore/entity/Match.java
@@ -1,8 +1,6 @@
 package com.cricinfo.livescore.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +14,8 @@ import java.util.Date;
 @AllArgsConstructor
 public class Match {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer matchId;
     private String teamHeading;
     private String matchNUmberVenue;
@@ -26,7 +26,16 @@ public class Match {
     private String liveText;
     private String matchLink;
     private String textComplete;
+
     @Enumerated
     private MatchStatus status;
     private Date date = new Date();
+
+    // set the match status according to textComplete
+    public void setMatchStatus() {
+        if (textComplete.isBlank())
+            this.status = MatchStatus.LIVE;
+        else
+            this.status = MatchStatus.COMPLETED;
+    }
 }
