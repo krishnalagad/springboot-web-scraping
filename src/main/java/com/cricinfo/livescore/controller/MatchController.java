c88b7b4e11a599df0626e1e20574fd0bf5b9d7bb
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-15 20:07:38 +0530

    Create MatchController.java

---
+++ src/main/java/com/cricinfo/livescore/controller/MatchController.java
@@ -0,0 +1,33 @@
+package com.cricinfo.livescore.controller;
+
+import com.cricinfo.livescore.entity.Match;
+import com.cricinfo.livescore.service.MatchService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/cric")
+public class MatchController {
+
+    private final MatchService matchService;
+
+    public MatchController(MatchService matchService) {
+        this.matchService = matchService;
+    }
+
+    @GetMapping("/all-matches")
+    public ResponseEntity<List<Match>> getAllMatches() {
+        List<Match> allMatches = this.matchService.getAllMatches();
+        return ResponseEntity.ok(allMatches);
+    }
+
+    @GetMapping("/live-matches")
+    public ResponseEntity<List<Match>> getLiveMatches() {
+        List<Match> liveMatches = this.matchService.getLiveMatches();
+        return ResponseEntity.ok(liveMatches);
+    }
+}
