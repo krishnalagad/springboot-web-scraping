39c433890642fe8c2a57805cd5664e5c30d2f1eb
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-15 18:24:28 +0530

    Update Match.java

--- src/main/java/com/cricinfo/livescore/entity/Match.java
+++ src/main/java/com/cricinfo/livescore/entity/Match.java
@@ -3,11 +3,17 @@ package com.cricinfo.livescore.entity;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Entity
 @Table(name = "cricket_match")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Match {
 
     private Integer matchId;
