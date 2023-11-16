354dcff7bae2b68b2e75260334bf094da53aef38
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-16 19:50:31 +0530

    - dump refresh button to refresh page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,8 +7,10 @@ import { RouterModule, RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [CommonModule, RouterOutlet, RouterModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'frontend';
+
+  handleRefreshClick() {}
 }
