2ef80addf0479ce1707544c72ec7c21d305ac1dc
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-16 11:39:29 +0530

    - dump environment variable for development and call apis

--- frontend/src/app/services/api-call.service.ts
+++ frontend/src/app/services/api-call.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
 
 @Injectable({
   providedIn: 'root'
@@ -9,15 +10,15 @@ export class ApiCallService {
   constructor(private _http: HttpClient) { }
 
   getAllMatches() {
-
+    return this._http.get(`${environment.apiUrl}/all-matches`);
   }
 
   getLiveMatches() {
-
+    return this._http.get(`${environment.apiUrl}/live-matches`)
   }
 
   getPointTable() {
-    
+    return this._http.get(`${environment.apiUrl}/point-table`)
   }
 
 }
