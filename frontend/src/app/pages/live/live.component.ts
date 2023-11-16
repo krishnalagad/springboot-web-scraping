a53660418a1d07053902eae3ed837b71cc17ac1a
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-16 13:30:59 +0530

    - dump live match component

--- frontend/src/app/pages/live/live.component.ts
+++ frontend/src/app/pages/live/live.component.ts
@@ -1,21 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ApiCallService } from '../../services/api-call.service';
+import { MatchCardComponent } from '../../components/match-card/match-card.component';
 
 @Component({
   selector: 'app-live',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatchCardComponent],
   templateUrl: './live.component.html',
   styleUrl: './live.component.css',
 })
 export class LiveComponent implements OnInit {
+
+  liveMatch: any;
   constructor(private _api: ApiCallService) {}
 
   ngOnInit(): void {
+    this.getLiveMatchData();
+  }
+
+  private getLiveMatchData() {
     this._api.getLiveMatches().subscribe({
       next: (data) => {
-        console.log(data);
+        this.liveMatch = data;
+        // console.log(data);
       },
       error: (err) => {
         console.log(err);
