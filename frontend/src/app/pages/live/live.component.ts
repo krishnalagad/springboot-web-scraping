ec921c70c52d8dbe59d8f9a97e8f5ddb5d2a2482
Krishna Lagad <krishnalagad2@gmail.com>
2023-11-16 11:49:25 +0530

    - dump api calling successfully. Data getting successfully.

--- frontend/src/app/pages/live/live.component.ts
+++ frontend/src/app/pages/live/live.component.ts
@@ -1,13 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ApiCallService } from '../../services/api-call.service';
 
 @Component({
   selector: 'app-live',
   standalone: true,
   imports: [CommonModule],
   templateUrl: './live.component.html',
-  styleUrl: './live.component.css'
+  styleUrl: './live.component.css',
 })
-export class LiveComponent {
+export class LiveComponent implements OnInit {
+  constructor(private _api: ApiCallService) {}
 
+  ngOnInit(): void {
+    this._api.getLiveMatches().subscribe({
+      next: (data) => {
+        console.log(data);
+      },
+      error: (err) => {
+        console.log(err);
+      },
+      complete: () => {
+        console.log('Request complete for live matches');
+      },
+    });
+  }
 }
